3cc964ab0fccb46b4c2c20badc3808d1d35241a2
anoush <anoush.atayan1991@gmail.com>
2020-03-24 13:44:11 +0400

    some styling changes

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
-    $('#submit_search').prop('disabled',true);
+    $('#submit_search').prop('disabled', true);
     $('#book').keyup(function() {
         $('#submit_search').prop('disabled', this.value == "" ? true : false);
     })
+
+    $('input[type=radio]').change(function() {
+         $('#submit_review').show();
+    });
 });
